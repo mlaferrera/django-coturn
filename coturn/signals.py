392fa3a6fd8d04b3f7cfa46ce764f79811250441
g-clef <aaron@avaloncyber.com>
2018-01-24 09:52:25 -0500

    fixing signals.py to properly handle child object methods, and updating README

--- coturn/signals.py
+++ coturn/signals.py
@@ -13,7 +13,19 @@ def sync_new_user_to_coturn(sender, instance, **kwargs):
             raise ImportError("Coturn was sent a signal from a non-django User model, but COTURN_USERNAME_FIELD is not set")
         if not hasattr(instance, settings.COTURN_USERNAME_FIELD):
             raise ValueError("Coturn - username field {} does not exist on model we were sent in sync signal".format(settings.COTURN_USERNAME_FIELD))
-        username = getattr(instance, settings.COTURN_USERNAME_FIELD)
+        if "." in settings.COTURN_USERNAME_FIELD:
+            # walk down the set of sub-fields until you reach the username
+            fields = settings.COTURN_USERNAME_FIELD.split(".")
+            working_instance = instance
+            for field in fields:
+                if hasattr(working_instance, field):
+                    working_instance = getattr(working_instance, field)
+                else:
+                    raise ValueError("missing sub-field in username search: {}".format(field))
+            username = working_instance()
+        else:
+            username = getattr(instance, settings.COTURN_USERNAME_FIELD)
+            username = username()
     if not hasattr(settings, "COTURN_REALM"):
         raise ValueError("Coturn - missing COTURN_REALM entry in settings.py")
     realm = settings.COTURN_REALM
