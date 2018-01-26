51b66072a989414ef2e3f856d8acda3808b5ea9e
g-clef <aaron@avaloncyber.com>
2018-01-26 10:08:07 -0500

    more forcing of proper db

--- coturn/management/commands/sync_coturn_shared_secret.py
+++ coturn/management/commands/sync_coturn_shared_secret.py
@@ -9,7 +9,7 @@ class Command(BaseCommand):
         secret = settings.COTURN_SHARED_SECRET
         if len(secret) > 127:
             raise Exception("Coturn's database doesn't support secrets longer than 127 characters")
-        for entry in TurnSecret.objects.all():
-            entry.delete()
+        for entry in TurnSecret.objects.using('coturn').all():
+            entry.delete(using="coturn")
         new_secret = TurnSecret(realm=realm, value=secret)
         new_secret.save(using="coturn")
