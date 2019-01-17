67d963e210b62f58cb88406801f28b8a6a1c6155
Marcus LaFerrera <mlaferrera@users.noreply.github.com>
2019-01-17 08:49:12 -0500

    Fix install from git repo
    Remove code to install from any directory since it breaks installing using pip from git repo

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ from setuptools import find_packages, setup
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
-
 setup(
     name='django-coturn',
     version='0.1',
